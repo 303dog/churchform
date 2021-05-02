5a97bcab777c65860e5516a9807185ebb8425830
Paul_Beckwith <303dog@gmail.com>
2021-05-01 19:40:46 -0600

    shipping formula added

--- src/component/AutoCalcForm.js
+++ src/component/AutoCalcForm.js
@@ -1,17 +1,18 @@
 import React from "react";
-import UploadFile from './UploadFile'
-import './AutoCalcForm.css'
+import "./AutoCalcForm.css";
+import Shipping from "./Shipping";
+import UploadFile from "./UploadFile";
 
 class AutoCalcForm extends React.Component {
   state = {
     products: [
-      { title: "Color XLarge", size: '12" X 18"', count: 0, price: 2.50 },
+      { title: "Color XLarge", size: '12" X 18"', count: 0, price: 2.5 },
       { title: "Color Large", size: '11" X 17"', count: 0, price: 1.75 },
       { title: "Color Medium", size: '8.5" X 11"', count: 0, price: 2.75 },
-      { title: "Color Small", size: '8.5" X 5.5"', count: 0, price: 2.50 },
+      { title: "Color Small", size: '8.5" X 5.5"', count: 0, price: 2.5 },
       { title: "Color XSmall", size: '4.25" X 5.5"', count: 0, price: 1.75 },
-      { title: "B&W XLarge", size: '12" X 18"', count: 0, price: 2.80 },
-      { title: "B&W Large", size: '11" X 17"', count: 0, price: 2.50 },
+      { title: "B&W XLarge", size: '12" X 18"', count: 0, price: 2.8 },
+      { title: "B&W Large", size: '11" X 17"', count: 0, price: 2.5 },
       { title: "B&W Medium", size: '8.5" X 11"', count: 0, price: 1.75 },
       { title: "B&W Small", size: '8.5" X 5.5"', count: 0, price: 2.75 },
       { title: "B&W XSmall", size: '4.25" X 5.5"', count: 0, price: 2.75 },
@@ -25,18 +26,26 @@ class AutoCalcForm extends React.Component {
       ),
     });
   };
-  
+
   render() {
     return (
       <>
-    <div className='product__box'>
-      <ProductList products={this.state.products} onChange={this.onChange} />
-      <Total products={this.state.products} />
-    <UploadFile />
-    </div>
-    </>
-  );
-}
+        <div className='product__box'>
+          <ProductList
+            products={this.state.products}
+            onChange={this.onChange}
+          />
+          <Total products={this.state.products} />
+          <Shipping
+            count={this.state.count}
+            title={this.state.title}
+            value={this.state.value}
+          />
+          <UploadFile />
+        </div>
+      </>
+    );
+  }
 }
 
 const ProductList = ({ products, onChange }) => (
@@ -56,9 +65,6 @@ const ProductList = ({ products, onChange }) => (
 );
 
 const Total = ({ products }) => (
-  <h3>
-    Price: $
-    {products.reduce((sum, i) => (sum += i.count * i.price), 0)}
-  </h3>
+  <h3>Price: ${products.reduce((sum, i) => (sum += i.count * i.price), 0)}</h3>
 );
 export default AutoCalcForm;
